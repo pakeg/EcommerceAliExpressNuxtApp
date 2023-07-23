0a43abb4e01f54ef99419b7ee98297ec57fef35a
pakeg <pakegpakeg@gmail.com>
2023-07-24 00:49:08 +0300

    Update CartItem.vue

--- components/CartItem.vue
+++ components/CartItem.vue
@@ -65,7 +65,9 @@ const userStore = useUserStore();
 
 const props = defineProps(["product, selectedArray"]);
 const { product, selectedArray } = toRefs(props);
-console.log(product)
+console.log(product, 'product')
+  console.log('selectedArray', selectedArray)
+  console.log(props, 'props')
 const emit = defineEmits(["selectedRadio"]);
 let isHover = ref(false);
 let isSelected = ref(false);
