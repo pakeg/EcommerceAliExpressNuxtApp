64cfa7e84ef932c137c4176701cc261e8ed4cead
pakeg <pakegpakeg@gmail.com>
2023-07-24 00:09:08 +0300

    Update shoppingcart.vue

--- pages/shoppingcart.vue
+++ pages/shoppingcart.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="ShoppingCartPage" class="mt-4 max-w-[1200px] mx-auto px-2">
     <div
-      v-if="!useUserStore.cart.length"
+      v-if="!userStore.cart.length"
       class="h-[500px] flex items-center justify-center"
     >
       <div class="pt-20">
