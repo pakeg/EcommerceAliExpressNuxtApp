09bedafbeced94280ff17b14423d3b2bfe998ada
Pakeg Bae <pakegpakeg@gmail.com>
2023-07-23 19:26:00 +0000

    Update nuxt.config.ts and package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-    "nuxt-icon",
-    "nuxt-lodash",
-    "@pinia/nuxt",
-    "@pinia-plugin-persistedstate/nuxt",
-    "@nuxtjs/tailwindcss",
-    "nuxtjs/supabase",
-  ],
-  runtimeConfig: {
-    public: {
-      stripePk: process.env.STRIPE_PK_KEY,
+    pages: true,
+    modules: [
+        'nuxt-icon',
+        'nuxt-lodash',
+        '@pinia/nuxt',
+        '@pinia-plugin-persistedstate/nuxt',
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/supabase'
+    ],
+    runtimeConfig: {
+        public: {
+          stripePk: process.env.STRIPE_PK_KEY
+        }
     },
-  },
-  app: {
-    head: {
-      script: [{ src: "https://js.stripe.com/v3/", defer: true }],
-    },
-  },
-});
+    app: {
+        head: {
+          script: [
+            { src: 'https://js.stripe.com/v3/', defer: true }
+          ],
+        }
+      }
+})
